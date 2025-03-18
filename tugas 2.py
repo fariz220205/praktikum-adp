2f4e3986f308331310eb0c396133114ac61469f2
fariz220205 <143883377+fariz220205@users.noreply.github.com>
2025-03-19 04:19:59 +0700

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,13 @@
+# program untuk mengestimasi luas daerah di bawah kurva f(x) = x^2 + 2x menggunakan metode riemann
+
+n=int(input("masukkan nilai n : "))
+Xa=1
+Xb=3
+delta_x=(Xb-Xa)/n
+luas=0
+for i in range(1, n+1):
+    Xi=Xa+i*delta_x
+    fXi=Xi**2+2*Xi
+    luas+=fXi
+luas*=delta_x
+print(f"Luas daerah di bawah kurva x^2 + 2x dengan batas daerah x=1 dan x=3 dan jumlah partisi n={n} adalah :{luas}")
